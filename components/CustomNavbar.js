7c29f8232b6a945e9f95e330dcbba44d577a1010
Jasper Rowan <jasper.rowan@berkeley.edu>
2022-01-20 15:48:12 -0800

    reverted image in nav bar to use img tag since next image component messed with styling. This will be something to fix later.

--- components/CustomNavbar.js
+++ components/CustomNavbar.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import Image from 'next/image'
 
 function CustomNavbar() {
     return (
@@ -10,11 +9,11 @@ function CustomNavbar() {
           <Navbar className="navBar" bg="dark" variant="dark" expand="sm">
             <Container className="innerNavbarContainer">
               <Navbar.Brand href="/">
-                <Image
+                <img
                   alt=""
                   src="/Gold Ore.png"
-                  width={30}
-                  height={30}
+                  width="30"
+                  height="30"
                   className="d-inline-block align-top"
                 />{" "}
                 NatureDAO
