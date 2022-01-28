c0ab688143df95d8e26f94c1bf461fe0bac7be09
Callam Ingram <callam@berkeley.edu>
2022-01-28 00:05:48 -0800

    Added an our mission line on the left side of the text. Edited the right side of the text. added a Goldflake image and applied to the right on hover.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import EmailBox from '../components/EmailBox';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import EmailBox from "../components/EmailBox";
 //  import { ScrollContainer, ScrollPage, Animator, Sticky } from 'react-scroll-motion';
-
 
 export default function Home() {
   return (
@@ -17,26 +16,49 @@ export default function Home() {
         </Row>
         <Row className={styles.row2}>
           <h1 className={styles.title}>
-            <strong>GOLD X CRYPTO</strong>
+            <strong className={styles.highlighted}>GOLD X CRYPTO</strong>
           </h1>
         </Row>
         <Row className={styles.row3}>
           <Col className={styles.col1} xs={12} sm={true}>
-            <h2 className={styles.paragraph}>
-
-            Digitizing <strong className={styles.highlighted} >conservation</strong> rights of unmined <strong className={styles.highlighted}>gold</strong>. Conservation is <strong className={styles.highlighted}>profitable</strong>. {" "}
-            </h2>
+            <Row>
+            <h6 className={styles.mission}>Our mission</h6>
+              <h2 className={styles.paragraph}>
+                Digitizing{" "}
+                <strong className={styles.highlighted}>conservation</strong>{" "}
+                rights of unmined{" "}
+                <strong className={styles.highlighted}>gold</strong>.
+                Conservation is{" "}
+                <strong className={styles.highlighted}>profitable</strong>.{" "}
+              </h2>
+            </Row>
           </Col>
           <Col className={styles.col2} xs={12} sm={true}>
             <p className={styles.paragraph}>
-            GreenVault aims to be a new take on the world's oldest asset class by <strong className={styles.highlighted} >tokenizing unmined gold</strong>. 
-            Proof of Work mining has detrimental negative effects on the environment however these effects are negligible compared to the effects of real world precious metal mining. 
-            Currently <strong className={styles.highlighted} >60-80%</strong> of all mined gold goes directly into a vault where it sits indefinitely transacted solely using TradFi. 
-            We propose a more <strong className={styles.highlighted} >environmentally friendly</strong> vision controlled solely using <strong className={styles.highlighted} >DeFi</strong>. 
-            By digitizing the conservation rights of this unmined gold and selling them, we can create a <strong className={styles.highlighted} >profit motive for conservation</strong> . 
+              GreenVault is a new take on the world's oldest asset class by{" "}
+              <strong className={styles.highlighted}>
+                tokenizing unmined gold
+              </strong>
+              . Proof of Work mining has detrimental negative effects on the
+              environment however these effects are negligible compared to the
+              effects of real world precious metal mining. Currently{" "}
+              <strong className={styles.highlighted}>60-80%</strong> of all
+              mined gold goes directly into a vault where it sits indefinitely
+              transacted solely using TradFi. We propose a more{" "}
+              <strong className={styles.highlighted}>
+                environmentally friendly
+              </strong>{" "}
+              vision controlled solely using{" "}
+              <strong className={styles.highlighted}>DeFi</strong>. By
+              digitizing the conservation rights of this unmined gold and
+              selling them, we can create a{" "}
+              <strong className={styles.highlighted}>
+                profit motive for conservation
+              </strong>{" "}
+              .
             </p>
           </Col>
-          <EmailBox/> 
+          <EmailBox />
         </Row>
       </Container>
     </>
