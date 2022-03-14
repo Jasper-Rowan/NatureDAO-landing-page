b74b715e6a6f3dd916070482b883a8d8324b9288
Jasper-Rowan <jasper.rowan@berkeley.edu>
2022-03-14 11:17:51 -0700

    pushing to merge

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import LandingPage from "../page-sections/LandingPage";
-import HomePage1 from "../page-sections/HomePage1";
 
 export default function Home() {
   return (
     <>
       <LandingPage />
-      <HomePage1 />
+      
     </>
   );
 }
