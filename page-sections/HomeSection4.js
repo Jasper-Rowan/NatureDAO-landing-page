9b8b9a05b98512e302d1e0e365dab0c6e1bc8fb6
Callam Ingram <callam@berkeley.edu>
2022-03-15 12:25:08 -0700

    fixed?

---
+++ page-sections/HomeSection4.js
@@ -0,0 +1,11 @@
+// import Carousel from 'react-native-snap-carousel';
+
+// const HomeSection4 = () => {
+//     return ( 
+//         <>
+//         <Carousel />
+//         </>
+//      );
+// }
+ 
+// export default HomeSection4;
