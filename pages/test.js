251237e513c328728cda17a010d690c9d9f1dfb9
Callam Ingram <callam@berkeley.edu>
2022-03-24 12:12:31 -0700

    made a /test page for working on integrations with web3 stuff.

--- pages/test.js
+++ pages/test.js
@@ -1,29 +1,84 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import styles from '../styles/test.module.css';
 
-// import { Web3ReactProvider } from '@web3-react/core'
-// import { Web3Provider } from "@ethersproject/providers";
+import { useWeb3React } from '@web3-react/core';
+import { InjectedConnector } from '@web3-react/injected-connector';
+import Web3 from 'web3';
 
-// function getLibrary(provider, connector) {
-//   return new Web3Provider(provider);
-// }
+
+
+export const injected = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42],
+})
 
 
 export default function Test() {
+    const {active, account, library, connector, activate, deactivate, provider} = useWeb3React();
+
+
+   
+    const web3 = new Web3(process.env.NEXT_PUBLIC_ETH_MAINNET);
+    // const web3 = provider; 
+
+    const [balance, setBalance] = useState(0); 
+
+
+
+    useEffect(() => {
+      // declare the data fetching function
+      const fetchData = async () => {
+        if (active) {
+
+          let data = await web3.eth.getBalance(account);
+
+          setBalance(Web3.utils.fromWei(data, 'ether'))
+          console.log(Web3.utils.fromWei(data, 'ether'))
+        }
+      }
+
+      // call the function
+      fetchData()
+        // make sure to catch any error
+        .catch(console.error);
+    }, [active])
+
+
+
+    async function connect() {
+
+      try {
+       await activate(injected);
+
+      } catch (ex) {
+        console.log(ex);
+      }
+    }
+
+
+    async function disconnect() {
+      try {
+        await deactivate();
+        setBalance(0);
+      } catch (ex) {
+        console.log(ex);
+      }
+    }
+
+    
 
     return (
-      <>
-        <Container className={styles.main} fluid>
-            <div> Network: </div> 
-            <div> Wallet Address: </div> 
-            <div> Account Balance: </div> 
-            <Button> Connect Wallet</Button>
-            {/* <Web3ReactProvider getLibrary={getLibrary}>
 
-            </Web3ReactProvider> */}
-        </Container>
-      </>
+
+        <div className={styles.main}>
+            <div> Network: <strong style={{color:"red"}}> {' '} {active.toString()} </strong> </div> 
+            <div> Wallet Address: {account} </div> 
+            <div> Account Balance: {balance} ETH </div> 
+
+            <Button onClick={connect}> Connect Wallet</Button>
+            <Button onClick={disconnect}> Disconnect Wallet</Button>
+
+        </div>
     );
 }
