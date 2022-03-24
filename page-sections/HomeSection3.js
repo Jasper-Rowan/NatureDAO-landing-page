8730c8f7efad64808fdb8281ba1ccaa0a1cceff5
Jasper-Rowan <jasper.rowan@berkeley.edu>
2022-03-23 20:17:15 -0700

    updated filler content on the about page

--- page-sections/HomeSection3.js
+++ page-sections/HomeSection3.js
@@ -12,14 +12,14 @@ const HomeSection3 = () => {
 
             <Row className={styles.row2}>
                 <Col lg={3} md={6} className={styles.col}> 
-                    <Tile image_link={"/BlueIcon.svg"} heading={"Liquidating Unmined Gold"} body={"Unmined gold has never been as valuable as it is right now"}/> 
+                    <Tile image_link={"/BlueIcon.svg"} heading={"Liquidating Unmined Gold"} body={"Unmined gold has never had a simple investment vechicle until now"}/> 
                         
                 </Col> 
                 <Col lg={3} md={6} className={styles.col}> 
-                    <Tile image_link={"/GreenIcon.svg"} heading={"No transaction fees"} body={"You can buy tokens without paying any transaction fee"}/> 
+                    <Tile image_link={"/GreenIcon.svg"} heading={"Low transaction fees"} body={"Using the cutting edge of blockchain technologies we can keep fees low"}/> 
                 </Col> 
                 <Col lg={3} md={6} className={styles.col}> 
-                    <Tile image_link={"/GrayIcon.svg"} heading={"Protect the environment"} body={"We have the infrastructure in place to protect 1,000,000 acres of land"}/> 
+                    <Tile image_link={"/GrayIcon.svg"} heading={"Protect the environment"} body={"We have the infrastructure in place to protect millions of arces of land"}/> 
                 </Col> 
                 <Col lg={3} md={6} className={styles.col}> 
                     <Tile image_link={"/GoldIcon.svg"} heading={"Security & Control"} body={"Provide high levels of security allowing users to keep assets secure"}/>
