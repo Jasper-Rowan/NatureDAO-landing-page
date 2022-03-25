60600642632fc1c3f10f7b2954803367cc18c3e7
Callam Ingram <callam@berkeley.edu>
2022-03-25 14:55:27 -0700

    potentially fixed issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,21 @@
 import Layout from '../components/Layout';
 import '../styles/globals.css';
-import Web3 from 'web3';
-import { Web3ReactProvider } from '@web3-react/core';
+// import Web3 from 'web3';
+// import { Web3ReactProvider } from '@web3-react/core';
 
 
-function getLibrary(provider) {
-  return new Web3(provider);
-}
+// function getLibrary(provider) {
+//   return new Web3(provider);
+// }
 
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
+    // <Web3ReactProvider getLibrary={getLibrary}>
     <Layout>
       <Component {...pageProps} />
     </Layout>
-    </Web3ReactProvider>
+    //</Web3ReactProvider>
   )
 }
 
--- pages/test.js
+++ pages/test.js
@@ -1,85 +1,85 @@
-import React, {useEffect, useState} from 'react'
-import { Button } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
-import styles from '../styles/test.module.css';
+// import React, {useEffect, useState} from 'react'
+// import { Button } from 'react-bootstrap';
+// import Container from 'react-bootstrap/Container';
+// import styles from '../styles/test.module.css';
 
-import { useWeb3React } from '@web3-react/core';
-import { InjectedConnector } from '@web3-react/injected-connector';
-import Web3 from 'web3';
-
-
-
-export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42],
-})
+// import { useWeb3React } from '@web3-react/core';
+// import { InjectedConnector } from '@web3-react/injected-connector';
+// import Web3 from 'web3';
 
 
-export default function Test() {
-    const {active, account, library, connector, activate, deactivate, provider} = useWeb3React();
+
+// export const injected = new InjectedConnector({
+//   supportedChainIds: [1, 3, 4, 5, 42],
+// })
+
+
+// export default function Test() {
+//     const {active, account, library, connector, activate, deactivate, provider} = useWeb3React();
 
 
    
-    const web3 = new Web3(process.env.NEXT_PUBLIC_ETH_MAINNET);
-    // const web3 = provider; 
+//     const web3 = new Web3(process.env.NEXT_PUBLIC_ETH_MAINNET);
+//     // const web3 = provider; 
 
-    const [balance, setBalance] = useState(0); 
-
-
-
-    useEffect(() => {
-      // declare the data fetching function
-      const fetchData = async () => {
-        if (active) {
-
-          let data = await web3.eth.getBalance(account);
-
-          setBalance(Web3.utils.fromWei(data, 'ether'))
-          console.log(Web3.utils.fromWei(data, 'ether'))
-        }
-      }
-
-      // call the function
-      fetchData()
-        // make sure to catch any error
-        .catch(console.error);
-    }, [active])
+//     const [balance, setBalance] = useState(0); 
 
 
 
-    async function connect() {
+//     useEffect(() => {
+//       // declare the data fetching function
+//       const fetchData = async () => {
+//         if (active) {
 
-      try {
-       await activate(injected);
+//           let data = await web3.eth.getBalance(account);
 
-      } catch (ex) {
-        console.log(ex);
-      }
-    }
+//           setBalance(Web3.utils.fromWei(data, 'ether'))
+//           console.log(Web3.utils.fromWei(data, 'ether'))
+//         }
+//       }
+
+//       // call the function
+//       fetchData()
+//         // make sure to catch any error
+//         .catch(console.error);
+//     }, [active])
+
+
+
+//     async function connect() {
+
+//       try {
+//        await activate(injected);
+
+//       } catch (ex) {
+//         console.log(ex);
+//       }
+//     }
+
+
+
+//     async function disconnect() {
+//       try {
+//         await deactivate();
+//         setBalance(0);
+//       } catch (ex) {
+//         console.log(ex);
+//       }
+//     }
 
     
 
-    async function disconnect() {
-      try {
-        await deactivate();
-        setBalance(0);
-      } catch (ex) {
-        console.log(ex);
-      }
-    }
-
-    
-
-    return (
+//     return (
 
 
-        <div className={styles.main}>
-            <div> Network: <strong style={{color:"red"}}> {' '} {active.toString()} </strong> </div> 
-            <div> Wallet Address: {account} </div> 
-            <div> Account Balance: {balance} ETH </div> 
+//         <div className={styles.main}>
+//             <div> Network: <strong style={{color:"red"}}> {' '} {active.toString()} </strong> </div> 
+//             <div> Wallet Address: {account} </div> 
+//             <div> Account Balance: {balance} ETH </div> 
 
-            <Button onClick={connect}> Connect Wallet</Button>
-            <Button onClick={disconnect}> Disconnect Wallet</Button>
+//             <Button onClick={connect}> Connect Wallet</Button>
+//             <Button onClick={disconnect}> Disconnect Wallet</Button>
 
-        </div>
-    );
-}
+//         </div>
+//     );
+// }
